91c17a2c9e771d4d762cc6e21d27a691b0d8bb63
Michael Jay Braga <majcbraga@gmail.com>
2017-04-17 23:40:02 +0800

    update on search.js

--- controllers/search.js
+++ controllers/search.js
@@ -2,11 +2,18 @@ var db = require(__dirname + '/../lib/mysql');
 
 exports.Search = function (req, res, next) {
   let searchQuery = req.params.searchQuery;
-  let sqlQuery = 'SELECT * FROM fiftyshadesofgrey where match(review) against (?);';
+  let sqlQuery = 'SELECT * FROM fiftyshadesofgrey where match(summary) against (?);';
 
   function action(err, rows) {
-    if(err) return (err);
-		res.status(200).send(rows);
+    if(err){
+      res.status(500).send({
+        'message':'Database error!',
+        'error': err
+      });
+    }
+    else{
+      res.status(200).send(rows);
+    }
   }
 
   db.query(sqlQuery, [searchQuery], action);
